9ed28160010df3899a679c106a484581488c7b1b
Dev Jijilal <djijilal@gmail.com>
2021-06-20 14:08:41 -0400

    Initial Commit

---
+++ HiNoteSummer21/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HiNoteSummer21
+//
+//  Created by Dev Jijilal on 6/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
