f357aa191ec8c8abaea25f78106216c8d30201f6
Dev Jijilal <djijilal@gmail.com>
2021-06-23 14:14:53 -0400

    Built review save+display. You can enter a song title in the 'write review section' press the button at the bottom to save the info. Next go to the 'view reviews' section. Press button at top to see the song title displayed. Definitely needs to be refined but concept is there.

--- HiNoteSummer21/ViewController.swift
+++ HiNoteSummer21/ViewController.swift
@@ -9,11 +9,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var songName: UITextField!
+    @IBOutlet weak var artist: UITextField!
+    @IBOutlet weak var album: UITextField!
+    @IBOutlet weak var review: UITextField!
+    @IBOutlet weak var rating: UITextField!
+    @IBOutlet weak var songNameView: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+    //    songName.delegate = self
+    //    artist.delegate = self
+  //      album.delegate = self
+  //      review.delegate = self
+  //      rating.delegate = self
+        
+        
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func enterTapped(_ sender: Any) {
+        
+        UserDefaults.standard.set(songName.text, forKey:
+                                  "songName")
+    }
+    
+    @IBAction func showReview(_ sender: Any) {
+        songNameView.text = UserDefaults.standard.object(forKey:
+        "songName") as? String
+    }
+ 
+    
+    
 
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        rating.resignFirstResponder()
+    }
+
+}
+
+extension ViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn( _ textField: UITextField) -> Bool {
+    textField.resignFirstResponder()
+    return true
+    }
 }
 
