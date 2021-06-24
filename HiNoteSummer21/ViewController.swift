2fe29c705d71d4b69e9fb79aecf39edb695395cc
Dev Jijilal <djijilal@gmail.com>
2021-06-24 12:51:55 -0400

    You can now write and view one review at a time

--- HiNoteSummer21/ViewController.swift
+++ HiNoteSummer21/ViewController.swift
@@ -15,6 +15,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var review: UITextField!
     @IBOutlet weak var rating: UITextField!
     @IBOutlet weak var songNameView: UILabel!
+    @IBOutlet weak var artistView: UILabel!
+    @IBOutlet weak var albumView: UILabel!
+    @IBOutlet weak var reviewView: UILabel!
+    @IBOutlet weak var ratingView: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,20 +38,38 @@ class ViewController: UIViewController {
         
         UserDefaults.standard.set(songName.text, forKey:
                                   "songName")
+        UserDefaults.standard.set(artist.text, forKey:
+                                  "artist")
+        UserDefaults.standard.set(album.text, forKey:
+                                  "album")
+        UserDefaults.standard.set(review.text, forKey:
+                                  "review")
+        UserDefaults.standard.set(rating.text, forKey:
+                                  "rating")
+        
+        
     }
     
     @IBAction func showReview(_ sender: Any) {
         songNameView.text = UserDefaults.standard.object(forKey:
         "songName") as? String
+        
+        artistView.text = UserDefaults.standard.object(forKey: "artist") as? String
+        
+        albumView.text = UserDefaults.standard.object(forKey: "album") as? String
+        reviewView.text = UserDefaults.standard.object(forKey: "review") as? String
+        ratingView.text = UserDefaults.standard.object(forKey: "rating") as? String
+        
+        
     }
  
     
     
 
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        rating.resignFirstResponder()
-    }
+ //   override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+ //       rating.resignFirstResponder()
+//    }
 
 }
 
