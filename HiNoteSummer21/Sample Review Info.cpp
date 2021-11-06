d2d613f12d0213d746493c0d22dcc67411e2fc05
Dev Jijilal <djijilal@gmail.com>
2021-11-06 18:13:23 -0400

    Added Objective C files with variable spots to store sample reviews data info. Also created bridging header to allow for both Objective C files and Swift files to exist and interact within the project.

---
+++ HiNoteSummer21/Sample Review Info.cpp
@@ -0,0 +1,16 @@
+//
+//  Sample Review Info.cpp
+//  HiNoteSummer21
+//
+//  Created by Dev Jijilal on 11/6/21.
+//
+
+#include "Sample Review Info.hpp"
+
+
+var "review number"
+var "song"
+var "artist"
+var "album"
+var "review"
+var "rating"
---
+++ HiNoteSummer21/Sample Review Info.hpp
@@ -0,0 +1,13 @@
+//
+//  Sample Review Info.hpp
+//  HiNoteSummer21
+//
+//  Created by Dev Jijilal on 11/6/21.
+//
+
+#ifndef Sample_Review_Info_hpp
+#define Sample_Review_Info_hpp
+
+#include <stdio.h>
+
+#endif /* Sample_Review_Info_hpp */
